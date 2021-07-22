bea494433afbab96fe25cc02276e27e46bcecd7c
P0PT4R7 <86586249+P0PT4R7@users.noreply.github.com>
2021-07-22 18:54:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 prompt ("There is no page here");
-
+prompt ("Literally there isnt a page here just an error");
+prompt ("It probably wont be fix so you can go on your way");
+userInput = false;
+while (!userInput)
+{
+  let userInput = prompt ("Oh you're still here")
+  if (userInput != 'yes')
+  {
+    alert("lies!")
+  }
+}    
+    
